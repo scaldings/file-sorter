7b27099aace8226d8ec1494e180b91f3a8d90186
Luke <lukas.zahoransky@zsbystricany.info>
2020-11-14 22:16:12 +0100

    Add files via upload

--- file-sorter.py
+++ file-sorter.py
@@ -1,1 +1,48 @@
-# Hello world! 
+import os
+from os.path import isfile, join
+from time import sleep
+import shutil
+
+
+def get_files(path: str):
+    files = []
+    for file in os.listdir(path):
+        if isfile(join(path, file)):
+            files.append(file)
+    return files
+
+
+def watch_folder(path: str):
+    last_files = get_files(path)
+    while True:
+        new_files = []
+        files = get_files(path)
+        for file in files:
+            if file not in last_files:
+                new_files.append(file)
+        if len(new_files) != 0:
+            return new_files
+        else:
+            sleep(5)
+
+
+def sort(files: list, path: str):
+    for file in files:
+        extension = f'.{file.split(".")[-1]}'
+        folders = os.listdir(path)
+        if extension not in folders:
+            os.mkdir(join(path, extension))
+        shutil.move(join(path, file), join(join(path, extension), file))
+        print(f'Moved {file} to {extension}/{file}')
+
+
+def main():
+    path = input('Enter the path of the folder you want to watch: ')
+    if '"' in path:
+        path = path.strip('"')
+    while True:
+        sort(watch_folder(path), path)
+
+
+if __name__ == "__main__":
+    main()
